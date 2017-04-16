111978d5574ce0385c0fff35697ad12db9b1d6de
Egor <mymailscan@mail.ru>
2017-04-16 20:32:11 +0300

    initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    [CustomAttribute]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object properties = typeof(Program).GetCustomAttributes(typeof(CustomAttribute), false)[0];
+            string name = ((CustomAttribute) properties).Name;
+            string email = ((CustomAttribute)properties).Email;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class CustomAttribute : System.Attribute
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+
+        public CustomAttribute()
+        {
+            Name = "Yahor Kavaliou";
+            Email = "Yahor_Kavaliou@epam.com";
+        }
+    }
+}
